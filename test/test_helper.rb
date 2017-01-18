c4382c0b680231a568ba565c8042e4aa991cffc9
Nicolas Rodriguez <nicolas.rodriguez@fraudbuster.mobi>
2017-01-18 17:41:32 +0100

    Fix CodeClimate error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,7 @@ require 'codeclimate-test-reporter'
 
 # Configure SimpleCov
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 
 SimpleCov.start do
