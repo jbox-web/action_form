1c8145120959b741e782027908dca6aa299d9bd1
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-18 23:30:06 +0100

    Remove useless generator

--- lib/rails/generators/form/templates/form.rb
+++
@@ -1,9 +0,0 @@
-<% module_namespacing do -%>
-class <%= class_name %>Form < ActionForm::Base
-<%- if attributes.present? -%>
-  attributes <%= attributes.map {|a| ":#{a.name}" }.join(", ") %>
-<%- else -%>
-  # attributes :name, :email
-<%- end -%>
-end
-<% end -%>
