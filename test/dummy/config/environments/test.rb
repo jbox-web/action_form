88ebe2e69b8e6d3842ede8b82102e8aa397197f8
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-19 05:55:58 +0100

    Cleanup dummy app

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -12,13 +12,13 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  if Rails.version[0] == '4'
+  # Configure public file server for tests with Cache-Control for performance.
+  if config.respond_to?(:public_file_server)
+    config.public_file_server.enabled = true
+    config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
+  else
     config.serve_static_files   = true
     config.static_cache_control = 'public, max-age=3600'
-  else
-    config.public_file_server.enabled = true
-    config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
   end
 
   # Show full error reports and disable caching.
@@ -36,12 +36,12 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Randomize the order test cases are executed.
+  config.active_support.test_order = :random
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Test order
-  config.active_support.test_order = :random
 end
