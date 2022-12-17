da960260aabcd9faa21bb617efdd34ee393dddef
Nicolas Rodriguez <nico@nicoladmin.fr>
2022-12-17 23:55:26 +0100

    Fix tests for Rails >= 7.0.3

--- lib/action_form/base.rb
+++ lib/action_form/base.rb
@@ -6,11 +6,15 @@ module ActionForm
     include FormHelpers
     extend ActiveModel::Callbacks
 
+    # Include ActiveModel::Validations::Callbacks so we can use *before_validation* in our forms
+    include ActiveModel::Validations::Callbacks
+
     define_model_callbacks :save, only: [:after]
     after_save :update_form_models
 
-    delegate :persisted?, :to_model, :to_key, :to_param, :to_partial_path, to: :model
     attr_reader :model, :forms
+
+    delegate :new_record?, :deleted?, :persisted?, :to_key, :to_param, :to_partial_path, to: :model
 
     def initialize(model)
       @model = model
@@ -37,6 +41,86 @@ module ActionForm
       else
         false
       end
+    end
+
+    class FormModelWrapper < SimpleDelegator
+      class FakeName
+        def initialize(model)
+          @klass = model.class
+        end
+
+        def param_key
+          ActiveModel::Naming.param_key(@klass)
+        end
+
+        def route_key
+          ActiveModel::Naming.route_key(@klass)
+        end
+
+        def singular_route_key
+          ActiveModel::Naming.singular_route_key(@klass)
+        end
+
+        def name
+          @klass.model_name.name
+        end
+
+        def human
+          @klass.model_name.human
+        end
+
+        def i18n_key
+          @klass.model_name.i18n_key
+        end
+      end
+
+      class << self
+
+        attr_reader :form, :model
+
+        def for(form:, model:, i18n_form: false)
+          @form  = form
+          @model = model
+          @i18n_form = i18n_form
+          new(form, model)
+        end
+
+        def i18n_form?
+          @i18n_form
+        end
+
+        def human_attribute_name(*args)
+          if i18n_form?
+            form.class.human_attribute_name(*args)
+          else
+            model.class.human_attribute_name(*args)
+          end
+        end
+
+        def validators_on(*args)
+          form.class.validators_on(*args)
+        end
+
+      end
+
+      def initialize(form, model)
+        @form = form
+        @model = model
+        super(@form)
+      end
+
+      delegate :new_record?, :deleted?, :persisted?,
+               :to_key, :to_param, :to_partial_path,
+               :==,
+               to: :@model
+
+      def model_name
+        FakeName.new(@model)
+      end
+    end
+
+    def to_model
+      FormModelWrapper.for(form: self, model: model)
     end
 
     class << self
