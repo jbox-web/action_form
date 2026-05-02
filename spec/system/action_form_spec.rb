136b05270a656005cc88f80d3ff3f1aa6271dd19
Nicolas Rodriguez <nico@nicoladmin.fr>
2026-05-02 02:05:54 +0200

    Fix Rubocop offenses

--- spec/system/action_form_spec.rb
+++ spec/system/action_form_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe 'ActionForm' do
   context 'when form is valid' do
     it 'renders html' do
       visit '/projects/new'
-      expect(page).to have_content('New project')
+      expect(page).to have_text('New project')
       fill_in :project_name, with: 'Project Foo'
       find('[type=submit]').click
-      expect(page).to have_content('A project has a name, and an owner')
-      expect(page).to have_content('Project Foo')
+      expect(page).to have_text('A project has a name, and an owner')
+      expect(page).to have_text('Project Foo')
     end
   end
 
   context 'when form is invalid' do
     it 'renders errors' do
       visit '/users/new'
-      expect(page).to have_content('New user')
+      expect(page).to have_text('New user')
       find('[type=submit]').click
-      expect(page).to have_content('7 errors prohibited this user from being saved')
+      expect(page).to have_text('7 errors prohibited this user from being saved')
     end
   end
 end
